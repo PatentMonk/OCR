406d82f32c488eedcc88253ca01902b6fd44b188
JordanRushing <Jordan@RushingFitness.com>
2015-02-21 23:51:59 -0500

    Optimize code

--- app/controllers/tesseract_pages_controller.rb
+++ app/controllers/tesseract_pages_controller.rb
@@ -2,27 +2,20 @@ class TesseractPagesController < ApplicationController
   skip_before_filter :verify_authenticity_token 
   def run
     require 'open-uri'
-  	puts "In Run Controller"
-
-    jpg = open("http://patentimages.storage.googleapis.com/#{params[:image]}.png").read
-    
-    puts "Creating directory"
     %x(mkdir tessdir)
+    contents = []
+    params[:images].each_with_index do |f,i|
+      file = File.open("tessdir/#{f.split('/')[0]}.jpg",'wb')
+      file.write open("http://patentimages.storage.googleapis.com/#{f}.png").read
+      
+      %x(tesseract tessdir/#{f.split('/')[0]}.jpg tessdir/out)
+      
+      file = File.open("tessdir/out.txt", "rb")
+      contents << {"#{i}" => file.read.scan(/\d{3}|Fig[ .]{1,}\d{1,}|Figure[ .]{1,}/i).uniq}
+    end
 
-    puts "Saving image"
-    file = File.open("tessdir/sample.jpg",'wb')
-  	file.write jpg
-	  
-    puts "Starting tesseract"
-    %x(tesseract tessdir/sample.jpg tessdir/out -l #{params[:language]})
-    
-    puts "Reading result"
-    file = File.open("tessdir/out.txt", "rb")
-    contents = file.read
-    
-    puts "removing tessdir"
     %x(rm -Rf tessdir)
     
-    render text: contents
+    render json: {strings: contents}
   end
 end
