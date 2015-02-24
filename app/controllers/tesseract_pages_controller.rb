ec3d8234df1069ea9035b3db91f1b5f3a5a8c39a
JordanRushing <Jordan@RushingFitness.com>
2015-02-24 11:46:02 -0500

    Fix file name duplication issue

--- app/controllers/tesseract_pages_controller.rb
+++ app/controllers/tesseract_pages_controller.rb
@@ -4,15 +4,15 @@ class TesseractPagesController < ApplicationController
     require 'open-uri'
     contents = []
     params[:images].each_with_index do |f,i|
-      
-      if !File.exist?("tessdir/#{f.split('/')[0]}.txt")
-        file = File.open("tessdir/#{f.split('/')[0]}.jpg",'wb')
+
+      if !File.exist?("tessdir/#{f.split('/')[1]}.txt")
+        file = File.open("tessdir/#{f.split('/')[1]}.jpg",'wb')
         file.write open("http://patentimages.storage.googleapis.com/#{f}.png").read
         
-        %x(tesseract tessdir/#{f.split('/')[0]}.jpg tessdir/#{f.split('/')[0]})
+        %x(tesseract tessdir/#{f.split('/')[1]}.jpg tessdir/#{f.split('/')[1]})
       end
       
-      file = File.open("tessdir/#{f.split('/')[0]}.txt", "rb")
+      file = File.open("tessdir/#{f.split('/')[1]}.txt", "rb")
       contents << {"#{i}" => file.read.split("\n").uniq.join(',').scan(/\d{3}[a-zA-Z]{1}|\d{3}|\d{2}[a-zA-Z]{1}|\d{2}|Fig[ .]{1,}\d{1,}|Figure[ .]{1,}/i).uniq}
     end
     
