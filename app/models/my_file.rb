9b06c31936ec88db823477edf44e7361919173c8
JordanRushing <Jordan@RushingFitness.com>
2015-02-26 22:58:31 -0500

    Fix even more things!

--- app/models/my_file.rb
+++ app/models/my_file.rb
@@ -6,30 +6,30 @@ class MyFile
 
     images.each_with_index do |f,i|
 
-      file = File.open("/tmp/#{f.split('/')[1]}.jpg",'wb')
-      file.write open("http://patentimages.storage.googleapis.com/#{f}.png").read
+      file = File.open("tessdir/#{f.split('/')[1]}.jpg",'wb')
+      file.write open(f).read
       
-      %x(tesseract /tmp/#{f.split('/')[1]}.jpg /tmp/#{f.split('/')[1]})
+      %x(tesseract tessdir/#{f.split('/')[1]}.jpg tessdir/#{f.split('/')[1]})
       
-      file = File.open("/tmp/#{f.split('/')[1]}.txt", "rb")
+      file = File.open("tessdir/#{f.split('/')[1]}.txt", "rb")
       contents << {"#{i}" => file.read.split("\n").uniq.join(',').scan(/\d{3}[a-zA-Z]{1}|\d{3}|\d{2}[a-zA-Z]{1}|\d{2}|Fig[ .]{1,}\d{1,}[a-zA-Z]{0,}|Figure[ .]{1,}/i).uniq}
     end
 
-    File.open("/tmp/#{f.split('/')[0]}.json", 'w') do |file| 
+    File.open("tessdir/#{f.split('/')[0]}.json", 'w') do |file| 
       file.write(contents.to_json)
     end
     if contents.present?
       create_s3("#{images[0].split('/')[0]}.json")
     end
-    %x(rm -fr /tmp/#{images[0].split('/')[1]}.txt)
-    %x(rm -fr /tmp/#{images[0].split('/')[0]}.json)
+    %x(rm -fr tessdir/#{images[0].split('/')[1]}.txt)
+    %x(rm -fr tessdir/#{images[0].split('/')[0]}.json)
     contents.to_json
   end
 
   def self.create_s3(filename)
     # upload to S3
     obj = $bucket.objects[filename]
-    obj.write(Pathname.new("/tmp/#{filename}"))
+    obj.write(Pathname.new("tessdir/#{filename}"))
   end
 
   def self.get_s3(filename)
